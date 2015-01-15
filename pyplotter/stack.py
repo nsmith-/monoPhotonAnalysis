7834097e2a8610273f253e2c928f66aa0ab9060f
Nick Smith <ncsmith6@wisc.edu>
2015-01-15 16:35:59 -0600

    stack plot

--- pyplotter/stack.py
+++ pyplotter/stack.py
@@ -7,21 +7,25 @@ fdata = ROOT.TFile("../data_selection.root")
 fqcd = ROOT.TFile("../qcd_selection.root")
 fznunu = ROOT.TFile("../znunu_selection.root")
 fwJets = ROOT.TFile("../wJets_selection.root")
+fsig = ROOT.TFile("../signal_selection.root")
 
 data = fdata.Get("EventTree")
 qcd = fqcd.Get("EventTree")
 znunu = fznunu.Get("EventTree")
 wJets = fwJets.Get("EventTree")
+signal = fsig.Get("EventTree")
 
 canvas = ROOT.TCanvas("canvas", "canvas")
 stack = ROOT.THStack("stack", "")
 
-hdata = ROOT.TH1F("data", "Data", 20, 100, 1200)
-hqcd = ROOT.TH1F("qcd", "QCD", 20, 100, 1200)
-hznunu = ROOT.TH1F("znunu", "Z#nu#nu", 20, 100, 1200)
-hwJets = ROOT.TH1F("wJets", "W+Jets", 20, 100, 1200)
+hdata = ROOT.TH1F("data", "Data", 20, 100, 1300)
+hqcd = ROOT.TH1F("qcd", "QCD", 20, 100, 1300)
+hznunu = ROOT.TH1F("znunu", "Z#nu#nu", 20, 100, 1300)
+hwJets = ROOT.TH1F("wJets", "W+Jets", 20, 100, 1300)
+hsignal = ROOT.TH1F("signal", "Signal", 20, 100, 1300)
 
 # what to draw
+label = "Photon ET"
 drawString = "phoEt[selectedPhoton]"
 cutString = ""
 
@@ -37,19 +41,39 @@ hznunu.SetFillColor(ROOT.kRed)
 wJets.Draw(drawString+">>wJets", cutString, "goff")
 hwJets.SetFillColor(ROOT.kGreen)
 
+signal.Draw(drawString+">>signal", cutString, "goff")
+hsignal.SetLineColor(ROOT.kRed)
+hsignal.SetLineStyle(ROOT.kDashed)
+
 # rescale MC
 hqcd.Scale(13/hqcd.Integral())
 hznunu.Scale(193/hznunu.Integral())
 hwJets.Scale(5/hwJets.Integral())
+hsignal.Scale(60/hsignal.Integral())
 
 bgDraw = ""
 stack.Add(hqcd, bgDraw)
 stack.Add(hwJets, bgDraw)
 stack.Add(hznunu, bgDraw)
+stack.Add(hsignal, bgDraw)
 
 stack.SetMaximum(120)
 stack.Draw()
-hdata.Draw("E0 same")
+hdata.Draw("E same")
+
+# Now, make pretty
+stack.GetXaxis().SetTitle(label)
+stack.GetYaxis().SetTitle("Events / 50 GeV")
+legend = ROOT.TLegend(0.7, 0.7, 0.95, 0.85)
+legend.AddEntry(hdata, "Data", "ep")
+legend.AddEntry(hqcd, "QCD", "f")
+legend.AddEntry(hznunu, "Z#nu#nu", "f")
+legend.AddEntry(hwJets, "W+Jets", "f")
+legend.AddEntry(hsignal, "Signal", "l")
+legend.SetBorderSize(0)
+legend.SetFillColor(0)
+legend.Draw()
+
 CMS_lumi.lumi_13TeV = "%s fb^{-1}" % 1
 iPos = 13
 CMS_lumi.writeExtraText = 1
