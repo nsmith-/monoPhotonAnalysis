43a4b068a67441f6cd1baab1b7f921e8f024efd2
Nick Smith <ncsmith6@wisc.edu>
2015-01-15 21:49:11 -0600

    uncertainty?

--- pyplotter/dphi_stack.py
+++ pyplotter/dphi_stack.py
@@ -1,27 +1,28 @@
 import ROOT
-import numpy
 import CMS_lumi, tdrstyle
 
 # what to draw
-label = "#slash{E}_{T} [GeV]"
-drawString = "pfMET"
-#label = "E_{T}^{#gamma} [GeV]"
-#drawString = "phoEt[selectedPhoton]"
+#label = "#Delta#phi (#gamma-#slash{E}_{T})"
+#drawString = "abs(deltaPhi_PhoMET)"
+label = "E_{T}^{#gamma} [GeV]"
+drawString = "phoEt[selectedPhoton]"
 cutString = ""
+#xrange = [0, 3.4]
+#yrange = [0, 0.5]
 xrange = [100, 1100]
-yrange = [0.1, 100]
-fill = True
-normalize = False
+yrange = [0.001, 1]
+fill = False
+normalize = True
 # qcd znunu wJet sig
 colors = [ROOT.kGreen-5, ROOT.kBlue-6, ROOT.kGray, ROOT.kRed]
 
 tdrstyle.setTDRStyle()
 
-fdata = ROOT.TFile("../data_selection.root")
-fqcd = ROOT.TFile("../qcd_selection.root")
-fznunu = ROOT.TFile("../znunu_selection.root")
-fwJets = ROOT.TFile("../wJets_selection.root")
-fsig = ROOT.TFile("../signal_selection.root")
+fdata = ROOT.TFile("~ncsmith/no_deltaphi_cut/data_selection.root")
+fqcd = ROOT.TFile("~ncsmith/no_deltaphi_cut/qcd_selection.root")
+fznunu = ROOT.TFile("~ncsmith/no_deltaphi_cut/znunu_selection.root")
+fwJets = ROOT.TFile("~ncsmith/no_deltaphi_cut/wJets_selection.root")
+fsig = ROOT.TFile("~ncsmith/no_deltaphi_cut/signal_selection.root")
 
 data = fdata.Get("EventTree")
 qcd = fqcd.Get("EventTree")
@@ -58,51 +59,39 @@ hsignal.SetLineColor(colors[3])
 hsignal.SetLineStyle(ROOT.kDashed)
 
 # rescale MC
-hqcd.Sumw2()
-hqcd.Scale(13/hqcd.Integral())
-hznunu.Sumw2()
-hznunu.Scale(193/hznunu.Integral())
-hwJets.Sumw2()
-hwJets.Scale(5/hwJets.Integral())
-hsignal.Scale(70/hsignal.Integral())
-bgTotal = [-1]
-for i in range(1,21) :
-  # first calculate uncertainty
-  bgTotal.append(hqcd.GetBinError(i)+hznunu.GetBinError(i)+hwJets.GetBinError(i))
+if ( normalize ) :
+  hqcd.Scale(1/hqcd.Integral())
+  hznunu.Scale(1/hznunu.Integral())
+  hwJets.Scale(1/hwJets.Integral())
+  hsignal.Scale(1/hsignal.Integral())
+  hdata.Sumw2()
+  hdata.Scale(1/hdata.Integral())
+else :
+  hqcd.Scale(13/hqcd.Integral())
+  hznunu.Scale(193/hznunu.Integral())
+  hwJets.Scale(5/hwJets.Integral())
+  hsignal.Scale(60/hsignal.Integral())
 
-# background uncertainty without affect stack total
-hbgUncertain = ROOT.TH1F("bgUncertain", "Background Uncertainty", 20, xrange[0], xrange[1])
-hbgUncertain.SetFillStyle(3013)
-hbgUncertain.SetFillColor(ROOT.kBlack)
-hbgUncertain.SetLineColor(ROOT.kWhite)
-for i in range(1,21) :
-  print i
-  hbgUncertain.SetBinContent(i, numpy.sqrt(bgTotal[i]))
-  hsignal.SetBinContent(i, hsignal.GetBinContent(i)-hbgUncertain.GetBinContent(i))
-
-bgDraw = "hist"
-stack.Add(hqcd, bgDraw)
-stack.Add(hwJets, bgDraw)
+bgDraw = ""
+#stack.Add(hqcd, bgDraw)
+#stack.Add(hwJets, bgDraw)
 stack.Add(hznunu, bgDraw)
-stack.Add(hbgUncertain, bgDraw)
 stack.Add(hsignal, bgDraw)
 
 stack.SetMinimum(yrange[0])
 stack.SetMaximum(yrange[1])
-#stack.Draw("nostack")
-stack.Draw()
-hdata.Draw("E same")
+stack.Draw("nostack")
+#stack.Draw()
+#hdata.Draw("E same")
 
 # Now, make pretty
 stack.GetXaxis().SetTitle(label)
 stack.GetYaxis().SetTitle("Events / 50 GeV")
-canvas.SetLogy(True)
-legend = ROOT.TLegend(0.65, 0.7, 0.95, 0.85)
-legend.AddEntry(hdata, "Data", "ep")
-legend.AddEntry(hqcd, "QCD", "f")
-legend.AddEntry(hznunu, "Z#nu#nu", "f")
-legend.AddEntry(hwJets, "W+Jets", "f")
-legend.AddEntry(hbgUncertain, "Bkg. Stat.", "f")
+legend = ROOT.TLegend(0.7, 0.7, 0.95, 0.85)
+#legend.AddEntry(hdata, "Data", "ep")
+#legend.AddEntry(hqcd, "QCD", "f")
+legend.AddEntry(hznunu, "Z#nu#nu", "l")
+#legend.AddEntry(hwJets, "W+Jets", "f")
 legend.AddEntry(hsignal, "SM+ADD(n_{ED}=2, M_{D}=3)", "l")
 legend.SetBorderSize(0)
 legend.SetFillColor(0)
